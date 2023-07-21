369a4c11c1eff47be18d903b4a9b370389ec8101
muhammaddaffafauzan <bayu_217@smkassalaambandung.sch.id>
2023-07-21 09:40:04 +0700

    baru update

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,35 @@ let seen = " "
 let a = 10;
 let b = 20;
 let c = a + b;
+// pengulangan
+let number = 1;
+let data = [
+  { 
+    nama: 'useng',
+   kelas: 'XII RPL 1',
+   alamat: 'bandung'
+  },
+  {
+   nama: 'asep',
+   kelas: 'XII RPL 2' ,
+   alamat: 'garut'
+  },
+  {
+   nama: 'agus',
+   kelas: 'XII TKR 2' ,
+   alamat: 'inggris'
+  },
+  {
+   nama: 'nanang',
+   kelas: 'XII TSM 2' ,
+   alamat: 'new york'
+  },
+  {
+   nama: 'mamat',
+   kelas: 'XI TKR 2' ,
+   alamat: 'gotham city'
+  },
+]
 </script>
 <template>
 <div>
@@ -24,9 +53,27 @@ let c = a + b;
  <p v-if="seen">kalo mau itu juga ya, gaji 3 digit 300 perak</p>
  <button :disabled="isButtonDisabled" onclick="alert('SELAMAT ANDA MASUK PEMUDA PANCASILA :)')">ya aku mau</button>&ensp;
  <button :disabled="isButtonDisabled" onclick="confirm('gamau tau kamu harus masuk aku maksa nih!!')">Gak mau</button><br>
- <a :href="url">ini link pendaftaran nya</a><hr>
+ <a :href="url">ini link pendaftaran nya</a>
+ {{ msg.split('').reverse().join(' ')
+}}<hr>
  <h1>{{ art }}</h1>
 <br> bilangan {{ a }} dijumlahkan bilangan {{ b }} maka hasilnya adalah {{ c }}
 </form>
+<hr>
+<h1>data siswa</h1>
+<table border="1" >
+  <tr>
+    <th>No</th>
+    <th>nama siswa</th>
+    <th>kelas</th>
+    <th>alamat</th>
+  </tr>
+  <tr v-for ="siswa in data">
+  <th>{{ number++ }}</th>
+  <td> {{ siswa.nama }} </td>
+  <td>{{ siswa.kelas }}</td>
+  <td>{{ siswa.alamat }}</td>
+  </tr>
+</table>
 </div>
 </template>
